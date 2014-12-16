78e2d3d12c6c3c51a2d7d522a6bc027da98fce8d
Xavi Magrinyà <xavi.magrinya@kyrolinc.com>
2014-12-16 17:21:15 +0200

    Refactor to avoid for loops. Documentation added

--- README.md
+++ README.md
@@ -3,3 +3,40 @@ ab-testing
 
 A/B testing made easy.
 
+## Install
+
+	npm install ab-testing
+
+## Initialize
+
+	var ABTesting = require('ab-testing');
+	
+	ABTesting.init([
+		{
+			name: 'A',
+			weight: 0.1
+		},
+		{
+			name: 'B',
+			weight: 0.9
+		}
+	])
+
+## Assign a group
+
+	// Give a unique ID for the user
+	var testGroup = ABTesting.getRandomGroup(user.id)
+	
+## A/B Test
+
+	ABTesting.test(testGroup, [
+        function () {
+            // Test A code here
+        },
+        function() {
+            // Test B code here
+        }
+    ], this)
+
+NB: the last parameter `this` is optional. If the scope is not required, this paremeter can be ignored.
+
--- tests/tests.js
+++ tests/tests.js
@@ -37,7 +37,7 @@ describe('Init', function() {
 
 describe('Test', function () {
     it('should execute the correct function for Alice', function() {
-        ABTesting.init([{ name : 'A', weight : 30 },{ name : 'B', weight: 30 }])
+        ABTesting.init([{ name : 'A' },{ name : 'B' }])
 
         var account = new Account({ email : 'alice@example.com', testGroup : ABTesting.getRandomGroup('alice@example.com') })
         ABTesting.test(account.testGroup, [
@@ -50,7 +50,7 @@ describe('Test', function () {
         ])
     })
     it('should execute the correct function for Bob', function() {
-        ABTesting.init([{ name : 'A', weight : 30 },{ name : 'B', weight: 30 }])
+        ABTesting.init([{ name : 'A' },{ name : 'B' }])
 
         var account = new Account({ email : 'bob@example.com', testGroup : ABTesting.getRandomGroup('bob@example.com') })
         ABTesting.test(account.testGroup, [
@@ -63,7 +63,7 @@ describe('Test', function () {
         ])
     })
     it('should execute with the correct scope', function() {
-        ABTesting.init([{ name : 'A', weight : 30 },{ name : 'B', weight: 30 }])
+        ABTesting.init([{ name : 'A' },{ name : 'B' }])
         this.foo = "foo"
         var account = new Account({ email : 'bob@example.com', testGroup : ABTesting.getRandomGroup('bob@example.com') })
         ABTesting.test(account.testGroup, [
